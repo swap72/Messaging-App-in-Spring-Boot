543a2395104af4a70f33b9c2d37d7b824d17f0af
Swapnil Mishra <swapnilmishra0923@gmail.com>
2024-06-27 19:44:50 +0530

    0th Commit

---
+++ src/test/java/com/ping/chat_app/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ping.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
